f1d1d637eb024c101b95a3d3c70094eb61a3c229
William Ashley <wash@amazon.com>
2024-05-05 14:46:31 -0500

    'go mod init' and tweaks to get builds (including cross-compilations) working

--- get_ldflags.sh
+++ get_ldflags.sh
@@ -15,7 +15,7 @@ set -eu
 build_flags=${1:-}
 if [[ -z $build_flags ]]; then
   # govvv define main.Version with the contents of ./VERSION file, if exists
-  build_flags=$(govvv -flags)
+  build_flags=$(GOOS="" GOARCH="" go run github.com/ahmetb/govvv@latest -flags)
 fi
 
 # you can add more flags here:
---
+++ go.mod
@@ -0,0 +1,8 @@
+module docopts/v2
+
+go 1.20
+
+require (
+	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
+	github.com/docopt/docopts v0.6.2
+)
